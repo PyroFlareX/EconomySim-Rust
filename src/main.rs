62baf22d21cd7cbb53344fcdbee9f68bae4b5eae
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2021-10-11 14:34:07 -0700

    Continuing economy loop stuff, ran into borrow checker issue I need to think about

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 
-use crate::Economy::{Country, GoodData, Province, State, WorldMarket};
+use Economy::EntityTag;
+
+use crate::Economy::{Country, EcoEntity, GoodData, Province, State, WorldMarket};
 
 mod Economy;
 
@@ -19,11 +21,9 @@ fn main() {
 
     let mut goods_map: HashMap<u8, GoodData> = HashMap::new();
 
-
     //Gotta load the goods, country, state, provinces, pops
     //Goods for now:
-    for i in 0..255
-    {
+    for i in 0..255 {
         let good = GoodData::new(i, 2.5, "good_name", 0);
         goods_map.insert(i, good);
     }
@@ -43,24 +43,67 @@ fn main() {
 
     let mut cont = true;
 
-    while cont
-    {
+    while cont {
         // First Clear from previous
         world_market.reset();
 
         // Then (in parallel too), create the demand lists for each country
-        
+        for (_country_id, country) in &mut country_map {
+            country.create_demand_lists();
+        }
 
         // Then (in parallel too) execute the local country economy tick
 
-
         // Then move the local economies into the world market (by rank)
-
+        //      Calculate the rank, this is not going to be done for this, since this is only partial, but /shrug
+        for (_country_id, country) in &mut country_map {
+            world_market.merge_country_market(country.get_market_mut());
+        }
 
         // Match remaining supply and demand inside the world economy (Pretty sure required to be sequential sadly)
         //      Within this, also do the pop ticks for growth, migration, assimilation, etc
+        for good_id in 0..255 {
+            for demand_recipt in &*world_market.get_market().get_good_demand_list(good_id) {
+                if demand_recipt.get_amount() <= 0.0 as f32 {
+                    continue;
+                }
 
-        
+                let mut remaining_demand = demand_recipt.get_amount();
+
+                let buyer_country = country_map
+                    .get_mut(&demand_recipt.get_tag().get_country_id())
+                    .expect("The country ID passed for the demand recipt is not valid.");
+
+                // let mut buyer =
+
+                for supply_recipt in world_market
+                    .get_market_mut()
+                    .get_good_supply_list_mut(good_id)
+                {
+                    if remaining_demand <= 0.0 {
+                        break;
+                    }
+                    if supply_recipt.get_amount() <= 0.0 {
+                        continue;
+                    }
+
+                    //Now here, I cross check between the available supply and the demand to match them to one another
+                    // supply_recipt.get_amount()
+                }
+            }
+        }
+        // Do the pop stuff
+        for (_country_id, country) in &mut country_map {
+            //The pop ticks for growth, immigration, etc. will happen here
+        }
+
         // Do any other thing that is required that I forgot
     }
 }
+/*
+fn getEconomicEntity(tag : &EntityTag) -> &Box<dyn EcoEntity>
+{
+    let p = Province::new(0, 0);
+
+    Box::new(&p)
+}*/
